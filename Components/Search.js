8880e5cf0f2d82daabeb220b17893ece5ed2cbb5
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2021-07-07 15:52:05 +0200

    first-customizing-comp

--- Components/Search.js
+++ Components/Search.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { View, TextInput, Button } from 'react-native';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
 
 export default class Search extends React.Component {
   render() {
     return (
-      <View>
-        <TextInput placeholder="Titre du Film"/>
+      <View style={{ marginTop: 20, backgroundColor: 'red' }}>
+        <TextInput
+          style={ styles.textinput }
+          placeholder="Titre du Film"
+        />
         <Button title="Rechercher" onPress={() => {}}/>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  }
+})
